5d77f4f5073137722af36b112b366cc3eb42bc1d
AlissonLewinski <lucasali7@gmail.com>
2021-06-01 09:58:01 -0300

    Improving rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   before { @user = FactoryBot.build(:user) }
 
   it { expect(@user).to respond_to(:email) }
-  it { expect(@user).to_not respond_to(:name) }
+  it { expect(@user).to respond_to(:name) }
   it { expect(@user).to respond_to(:password) }
   it { expect(@user).to respond_to(:password_confirmation) }
   it { expect(@user).to be_valid }
